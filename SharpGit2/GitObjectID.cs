2b2da8a743a5b56d4995a9841f659f6da7a46b29
Gamma_Draconis <17joshuanewcomb@gmail.com>
2024-07-16 16:13:56 -0700

    Git Object ID

---
+++ SharpGit2/GitObjectID.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpGit2;
+
+public unsafe struct GitObjectID
+{
+    public fixed byte Id[20];
+}
