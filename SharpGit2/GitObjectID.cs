9852d097ba4f5b972eb68997f7fd5fe584361907
Gamma_Draconis <17joshuanewcomb@gmail.com>
2024-07-24 11:52:52 -0700

    Ugly support for experimental SHA256 object id

--- SharpGit2/GitObjectID.cs
+++ SharpGit2/GitObjectID.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace SharpGit2;
 
-public unsafe struct GitObjectID
+#if SHA256_OBJECT_ID
+public enum GitObjectIDType : byte
 {
-    public fixed byte Id[20];
+    SHA1 = 1,
+    SHA256 = 2
+}
+#endif
+
+[StructLayout(LayoutKind.Sequential)]
+public struct GitObjectID
+{
+#if SHA256_OBJECT_ID
+    public GitObjectIDType Type;
+#endif
+    public IdArray Id;
+
+#if SHA256_OBJECT_ID
+    [InlineArray(32)]
+#else
+    [InlineArray(20)]
+#endif
+    public struct IdArray
+    {
+        public byte Element;
+    }
 }
