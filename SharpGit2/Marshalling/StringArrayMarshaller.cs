bcd1ef5e10016dd9af103c0aef0ce6e3550127bb
Gamma_Draconis <17joshuanewcomb@gmail.com>
2024-08-09 15:02:54 -0700

    Simplify string array marshaller

--- SharpGit2/Marshalling/StringArrayMarshaller.cs
+++ SharpGit2/Marshalling/StringArrayMarshaller.cs
@@ -8,67 +8,78 @@ namespace SharpGit2.Marshalling;
 [CustomMarshaller(typeof(string[]), MarshalMode.ManagedToUnmanagedIn, typeof(ManagedToUnmanagedIn))]
 internal unsafe static class StringArrayMarshaller
 {
+    public static Git2.StringArray ConvertToUnmanaged(string[] array)
+    {
+        if (array is null or { Length: 0 })
+            return default;
+
+        byte* strMemTmp = null;
+        int countInitialized = 0;
+
+        var arrayMemory = (byte**)NativeMemory.Alloc((nuint)array.Length * (nuint)sizeof(void*));
+        
+        try
+        {
+            for (int i = 0; i < array.Length; ++i, ++countInitialized)
+            {
+                string value = array[i];
+
+                if (string.IsNullOrEmpty(value))
+                {
+                    arrayMemory[i] = null;
+                    continue;
+                }
+
+                int exactCount = Encoding.UTF8.GetByteCount(value);
+
+                byte* strMem = strMemTmp = (byte*)NativeMemory.Alloc((nuint)exactCount + 1);
+                
+                int written = Encoding.UTF8.GetBytes(value, new Span<byte>(strMem, exactCount));
+                strMem[written] = 0;
+                arrayMemory[i] = strMem;
+
+                strMemTmp = null;
+            }
+
+            return new Git2.StringArray(arrayMemory, (nuint)array.Length);
+        }
+        catch
+        {
+            if (strMemTmp is not null)
+            {
+                NativeMemory.Free(strMemTmp);
+            }
+
+            for (int j = 0; j < countInitialized; ++j)
+            {
+                NativeMemory.Free(arrayMemory[j]);
+            }
+
+            NativeMemory.Free(arrayMemory);
+            throw;
+        }
+    }
+
+    public static void Free(Git2.StringArray array)
+    {
+        if (array.Strings is null)
+            return;
+
+        for (nuint i = 0; i < array.Count; ++i)
+        {
+            NativeMemory.Free(array.Strings[i]);
+        }
+
+        NativeMemory.Free(array.Strings);
+    }
+
     public ref struct ManagedToUnmanagedIn
     {
         private Git2.StringArray _array;
 
         public void FromManaged(string[] array)
         {
-            if (array is null or { Length: 0 })
-                return;
-
-            var memory = NativeMemory.AllocZeroed((nuint)array.Length * (nuint)sizeof(void*));
-
-            try
-            {
-                Span<nint> span = new(memory, array.Length);
-
-                int i = 0;
-
-                try
-                {
-                    for (; i < array.Length; ++i)
-                    {
-                        string value = array[i];
-
-                        if (value is null)
-                            continue; // span element should already be zero
-
-                        int exactCount = Encoding.UTF8.GetByteCount(value);
-
-                        byte* strMem = (byte*)NativeMemory.Alloc((nuint)exactCount + 1);
-
-                        try
-                        {
-                            int written = Encoding.UTF8.GetBytes(value, new Span<byte>(strMem, exactCount));
-                            strMem[written] = 0;
-
-                            span[i] = (nint)strMem;
-                        }
-                        catch
-                        {
-                            NativeMemory.Free(strMem);
-                            throw;
-                        }
-                    }
-                }
-                catch
-                {
-                    for (int j = 0; j < i; ++j)
-                    {
-                        NativeMemory.Free((byte*)span[j]);
-                    }
-
-                    throw;
-                }
-            }
-            catch
-            {
-                NativeMemory.Free(memory);
-                throw;
-            }
-
-            _array = new Git2.StringArray((byte**)memory, (nuint)array.Length);
+            _array = ConvertToUnmanaged(array);
         }
 
         public Git2.StringArray* ToUnmanaged()
@@ -78,18 +89,7 @@ internal unsafe static class StringArrayMarshaller
 
         public void Free()
         {
-            if (_array.Strings is null)
-                return;
-
-            var span = new ReadOnlySpan<nint>(_array.Strings, (int)_array.Count);
-
-            for (int i = 0; i < span.Length; ++i)
-            {
-                NativeMemory.Free((void*)span[i]); // null is handled into being a no-op
-            }
-
-            NativeMemory.Free(_array.Strings);
-
+            StringArrayMarshaller.Free(_array);
             _array = default;
         }
     }
