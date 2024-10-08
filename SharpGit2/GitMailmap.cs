ab7f5f70d66b61d824cad37c2fd3147ebb094705
Gamma_Draconis <17joshuanewcomb@gmail.com>
2024-10-08 15:08:30 -0700

    Add GitMailMap

---
+++ SharpGit2/GitMailmap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static SharpGit2.NativeApi;
+
+namespace SharpGit2;
+
+public unsafe readonly struct GitMailmap(Git2.MailMap* nativeHandle) : IDisposable
+{
+    public readonly Git2.MailMap* NativeHandle = nativeHandle;
+
+    public void Dispose()
+    {
+        git_mailmap_free(this.NativeHandle);
+    }
+}
