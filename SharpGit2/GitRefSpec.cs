fa9dab7326e104c5821c9cb7d80c5ad2236102b6
Gamma_Draconis <17joshuanewcomb@gmail.com>
2024-10-25 14:04:48 -0700

    GitRefSpec initial definition

---
+++ SharpGit2/GitRefSpec.cs
@@ -0,0 +1,13 @@
+﻿using static SharpGit2.NativeApi;
+
+namespace SharpGit2;
+
+public unsafe readonly struct GitRefSpec(Git2.RefSpec* nativeHandle) : IDisposable
+{
+    public readonly Git2.RefSpec* NativeHandle = nativeHandle;
+
+    public void Dispose()
+    {
+        git_refspec_free(this.NativeHandle);
+    }
+}
